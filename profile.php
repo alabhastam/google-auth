471522eab3857deda378a30d80d5ed9c5425cd51
alitorpedo <abdollahia713@gmail.com>
2024-09-26 21:13:13 +0330

    db_conf

--- profile.php
+++ profile.php
@@ -7,10 +7,29 @@ if (!isset($_SESSION['google_loggedin'])) {
     exit;
 }
 // Retrieve session variables
+// Database connection variables
+$db_host = 'localhost';
+$db_name = 'sociallogin';
+$db_user = 'root';
+$db_pass = '';
+// Attempt to connect to database
+try {
+    // Connect to the MySQL database using PDO...
+    $pdo = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    // Could not connect to the MySQL database, if this error occurs make sure you check your db settings are correct!
+    exit('Failed to connect to database!');
+}
+// Get the account from the database
+$stmt = $pdo->prepare('SELECT * FROM accounts WHERE id = ?');
+$stmt->execute([ $_SESSION['google_id'] ]);
+$account = $stmt->fetch(PDO::FETCH_ASSOC);
+// Retrieve session variables
 $google_loggedin = $_SESSION['google_loggedin'];
-$google_email = $_SESSION['google_email'];
-$google_name = $_SESSION['google_name'];
-$google_picture = $_SESSION['google_picture'];
+$google_email = $account['email'];
+$google_name = $account['name'];
+$google_picture = $account['picture'];
 ?>
 
 <!DOCTYPE html>
